47f2a14ab43a9d69c721c4f68412cb5300b5731c
Iliya <verghel.iliya@gmail.com>
2023-11-20 23:49:50 +0300

    statick work

--- SP3/mainProgram/mainProgram/mainProgram.cpp
+++ SP3/mainProgram/mainProgram/mainProgram.cpp
@@ -1,6 +1,16 @@
-﻿#include <iostream>
+﻿#include <Windows.h>
+#include <iostream>
+#include "dll.h"
 
 int main()
 {
     std::cout << "Hello World!\n";
+    DWORD processId = GetCurrentProcessId();
+
+    const char strForSearch[] = "why am i doing this";
+    const char strReplace[] = "for this";
+
+    std::cout << "Befor: " << strForSearch << std::endl;
+    ReplaceStr(processId, strForSearch, strReplace);
+    std::cout << "After: " << strForSearch << std::endl;
 }
