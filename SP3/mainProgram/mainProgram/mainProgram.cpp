dc45496b324e2f2556335bb76e1c4699d12a750c
Iliya <verghel.iliya@gmail.com>
2023-11-21 00:22:27 +0300

    dynamic done

--- SP3/mainProgram/mainProgram/mainProgram.cpp
+++ SP3/mainProgram/mainProgram/mainProgram.cpp
@@ -2,15 +2,50 @@
 #include <iostream>
 #include "dll.h"
 
+typedef void __stdcall TDynamicReplace(DWORD, const char*, const char*);
+
+void ReplaceStrDynamic(DWORD processId, const char* strForSearch, const char* strReplace);
+
 int main()
 {
-    std::cout << "Hello World!\n";
     DWORD processId = GetCurrentProcessId();
 
     const char strForSearch[] = "why am i doing this";
     const char strReplace[] = "for this";
 
+    
+    char method = '1';
+    //std::cin >> method;
     std::cout << "Befor: " << strForSearch << std::endl;
-    ReplaceStr(processId, strForSearch, strReplace);
+    switch (method)
+    {
+    case '1':
+        ReplaceStr(processId, strForSearch, strReplace);
+        break;
+    case '2':
+        ReplaceStrDynamic(processId, strForSearch, strReplace);
+        break;
+    case '3':
+        ReplaceStrDynamic(processId, strForSearch, strReplace);
+        break;
+    default:
+        break;
+    }    
     std::cout << "After: " << strForSearch << std::endl;
 }
+
+void ReplaceStrDynamic(DWORD processId, const char* strForSearch, const char* strReplace)
+{
+    HINSTANCE hLibrary = LoadLibrary(TEXT("DllReplace.dll"));
+
+    if (hLibrary != NULL)
+    {
+        TDynamicReplace* DynamicReplace = (TDynamicReplace*)GetProcAddress(hLibrary, "ReplaceStr");
+        if (DynamicReplace != NULL)
+        {
+            DynamicReplace(processId, strForSearch, strReplace);
+        }
+        FreeLibrary(hLibrary);
+    }
+}
+
